992accf54e1690083ddbad7cebfbef3cf7a57b21
harvy <harvinder.atwal@cdl.co.uk>
2014-01-10 17:13:18 +0000

    Read in from console via gradle

--- Battleships/src/main/java/uk/co/lala/battleships/game/Game.java
+++ Battleships/src/main/java/uk/co/lala/battleships/game/Game.java
@@ -1,11 +1,32 @@
 package uk.co.lala.battleships.game;
 
+import java.io.BufferedReader;
+import java.io.Console;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Game {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Ocean oc = new Ocean();
-		ShipFactory shipFactory = new ShipFactory(); 
+		//Ocean oc = new Ocean();
+		//ShipFactory shipFactory = new ShipFactory(); 
+        
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("Please enter user name : ");
+        String username = null;
+        try {
+            username = reader.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } 
+        
+	     Console console = System.console();
+	     username = console.readLine("Please enter user name : ");   
+	     System.out.println("You entered : " + username);
+	     
+	     
+		
+		
 	}
 
 }
