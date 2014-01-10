81150036ceb77e2f678e02529cd3598384511a79
harvy <harvinder.atwal@cdl.co.uk>
2014-01-10 16:57:51 +0000

    Creating main game class

---
+++ Battleships/src/main/java/uk/co/lala/battleships/game/Game.java
@@ -0,0 +1,11 @@
+package uk.co.lala.battleships.game;
+
+public class Game {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Ocean oc = new Ocean();
+		ShipFactory shipFactory = new ShipFactory(); 
+	}
+
+}
