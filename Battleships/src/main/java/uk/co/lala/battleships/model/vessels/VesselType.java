6684c7ef2ba47028be222936355781a5529abfda
unknown <stevenh01@STEVENH01-LAP1.cheshdatasys.co.uk>
2014-01-13 15:01:59 +0000

    updated to include sizes and added CAT_CARRIER for CatCarrier class

--- Battleships/src/main/java/uk/co/lala/battleships/model/vessels/VesselType.java
+++ Battleships/src/main/java/uk/co/lala/battleships/model/vessels/VesselType.java
@@ -1,11 +1,23 @@
 package uk.co.lala.battleships.model.vessels;
 
-
 /**
  * Enum describing what various types of vessels are available for {@link Ship}s
+ * 
  * @author joshuas
- *
+ * 
  */
 public enum VesselType {
-	SCOUT, AIRCRAFT_CARRIER, FRIGATE, DESTROYER, DREADNOUGHT;
+	SCOUT(1), AIRCRAFT_CARRIER(5), FRIGATE(3), DESTROYER(2), DREADNOUGHT(4), CAT_CARRIER(
+			2);
+
+	private final int size;
+
+	private VesselType(int size) {
+		this.size = size;
+	};
+
+	public int getSize() {
+		return size;
+	}
+
 }
