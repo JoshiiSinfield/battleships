9a828804fd6f36cd3a4079127a21145aeb9b54de
steven hart <steven.hart.1986@gmail.com>
2014-01-13 15:36:49 +0000

    I don't see how you can determine worldSize with x*y, it assume that the user will pass a square in without enforcing it. I prepose the following to allow a rectangle world to be created

--- Battleships/src/main/java/uk/co/lala/battleships/game/World.java
+++ Battleships/src/main/java/uk/co/lala/battleships/game/World.java
@@ -1,16 +1,19 @@
 package uk.co.lala.battleships.game;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import uk.co.lala.battleships.model.world.WorldObject;
-
 public class World {
-	
-	public World(int x, int y){
-		int worldSize = x * y;
-		List<WorldObject> listOfWorldObjects = new ArrayList<WorldObject>(worldSize);
+
+	private int[][] worldMap;
+
+	public World(int x, int y) {
+		this.setWorldMap(new int[x][y]);
 	}
 
-	
+	public int[][] getWorldMap() {
+		return worldMap;
+	}
+
+	public void setWorldMap(int[][] worldMap) {
+		this.worldMap = worldMap;
+	}
+
 }
